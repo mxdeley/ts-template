b315ed24c8dea429326c032badf7d2983ef5fc1f
mxdeley <dev@madeleydesignstudio.org>
2024-06-26 21:26:35 +0100

    added lemon squeezy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
 "use client";
 import { useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
+import axios from "axios";
 
 export default function Home() {
   const tasks = useQuery(api.tasks.get);
+  const buyProduct1 = async () => {
+    try {
+      const response = await axios.post("/api/purchaseProduct", {
+        productId: "430198",
+      });
+      console.log(response.data);
+      window.open(response.data.checkoutUrl, "_blank");
+    } catch (error) {
+      console.error(error);
+      alert("An error occured");
+    }
+  };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+      <div> {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}</div>
+      <div>
+        <button onClick={buyProduct1}>Buy Product 1</button>
+      </div>
     </main>
   );
 }
